a6eabe2c1837fff8da44e04cd21c1751321e48e2
Harsh <hvaghela141@gmail.com>
2023-05-18 13:09:57 +0530

    Last Commit

--- PyPractice.py
+++ PyPractice.py
@@ -1,16 +1,6 @@
 from flask import Flask
-
-# Create an instance of the Flask class that is the WSGI application.
-# The first argument is the name of the application module or package,
-# typically __name__ when using a single module.
 app = Flask(__name__)
 
-# Harsh And Vivek Project
-
-
-# Flask route decorators map / and /hello to the hello function.
-# To add other resources, create functions that generate the page contents
-# and add decorators to define the appropriate resource locators for them.
 
 @app.route('/')
 @app.route('/hello')
@@ -19,7 +9,7 @@ def hello():
     # a = input("What's your name? : ")
     # b = str("Hey! Welcome " + a + "! You're Awesome!")
     # return b
-    return "Hello! Welcome to Simpl_Py_App!"
+    return "Hello! Welcome to Simple Python App!"
 
 # """Run the app server on localhost:4449"""
 # from waitress import serve
